2f81176812e97dbc68829b50c06daac2652c65b1
Abdul Rauf Afzal <raufafzal400@gmail.com>
2026-08-03 15:40:54 +0500

    updated

--- src/Section/Tenant/DispatchBadges/components/DispatchStats.jsx
+++ src/Section/Tenant/DispatchBadges/components/DispatchStats.jsx
@@ -53,7 +53,7 @@ const DispatchStats = ({ summary = {} }) => {
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-2">
       {cards.map((item) => (
         <Card
           key={item.id}
